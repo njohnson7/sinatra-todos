004c75bfccfef04b0c1436220a2e53113e5afff4
Nick Johnson <mienaineko@gmail.com>
2017-06-01 09:43:53 -0700

    added some javascript

---
+++ public/javascripts/application.js
@@ -0,0 +1,33 @@
+$(function() {
+
+  $('form.delete').submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm('Are you sure? This cannot be undone!');
+    if (ok) {
+      var form = $(this);
+
+      var request = $.ajax({
+        url: form.attr('action'),
+        method: form.attr('method')
+      });
+
+      request.done(function(data, textStatus, jqHXR) {
+        if (jqHXR.status == 204) {
+          form.parent('li').remove()
+        } else if (jqHXR.status == 200) {
+          document.location = data;
+        }
+      });
+    }
+  });
+});
+
+// $(document).ready(function() {
+//   $('.delete button, button.delete').click(function() {
+//     if (!confirm('Are you sure?')) {
+//       return false
+//     }
+//   });
+// });
--- todo.rb
+++ todo.rb
@@ -6,6 +6,7 @@ require 'sinatra/reloader' if development?
 configure do
   enable :sessions
   set :session_secret, 'secret'
+  set :erb, escape_html: true
 end
 
 helpers do
@@ -34,11 +35,16 @@ helpers do
   end
 
   def sort_todos(todos, &block)
-    sort(todos) { |todo, _| todo[:completed] ? 1 : 0 }.each(&block)
+    # sort(todos) { |todo, _| todo[:completed] ? 1 : 0 }.each(&block)
+    todos.sort_by { |todo| todo[:completed] ? 1 : 0 }.each(&block)
   end
 
   def sort(arr, &block)
     arr.map.with_index { |elem, idx| [elem, idx] }.sort_by(&block)
+  end
+
+  def h(content)
+    Rack::Utils.escape_html(content)
   end
 end
 
@@ -49,6 +55,21 @@ end
 get '/' do
   redirect '/lists'
 end
+
+def load_id_and_list
+  list_id = params[:list_id]
+  unless list_id =~ /\A\d+\z/ && (0...session[:lists].size).cover?(list_id.to_i)
+    session[:error] = 'The specified list was not found.'
+    redirect '/lists'
+  end
+  list_id = list_id.to_i
+  list = session[:lists][list_id]
+  [list_id, list]
+end
+
+# get '/list_id_error' do
+#   "Invalid list ID: #{params[:list_id].inspect}"
+# end
 
 # View list of lists
 get '/lists' do
@@ -90,26 +111,22 @@ post '/lists' do
   end
 end
 
+# View a single todo list
 get '/lists/:list_id' do
-  @list_id = params[:list_id].to_i
-  redirect '/lists' unless (0...session[:lists].size).cover?(@list_id)
-  @list = session[:lists][@list_id]
-
+  @list_id, @list = load_id_and_list
   erb :list, layout: :layout
 end
 
 # Edit an existing todo list
 get '/lists/:list_id/edit' do
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
   erb :edit_list, layout: :layout
 end
 
 # Update an existing todo list
 post '/lists/:list_id' do
   list_name = params[:list_name].strip
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
 
   error = error_for_list_name(list_name)
   if error
@@ -124,16 +141,24 @@ end
 
 # Delete a todo list
 post '/lists/:list_id/delete' do
-  @list_id = params[:list_id].to_i
+  @list_id = load_id_and_list.first
   session[:lists].delete_at(@list_id)
-  session[:success] = 'The list has been deleted.'
-  redirect '/lists'
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
+    '/lists'
+  else
+    session[:success] = 'The list has been deleted.'
+    redirect '/lists'
+  end
+end
+
+def next_todo_id(todos)
+  max = todos.map { |todo| todo[:id] }.max || -1
+  max + 1
 end
 
 # Add a new todo to a list
 post '/lists/:list_id/todos' do
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
   text = params[:todo].strip
 
   error = error_for_todo(text)
@@ -141,7 +166,10 @@ post '/lists/:list_id/todos' do
     session[:error] = error
     erb :list, layout: :layout
   else
-    @list[:todos] << { name: text, completed: false }
+
+    id = next_todo_id(@list[:todos])
+    @list[:todos] << { id: id, name: text, completed: false }
+
     session[:success] = 'The todo was added.'
     redirect "/lists/#{@list_id}"
   end
@@ -149,24 +177,26 @@ end
 
 # Delete a todo from a list
 post '/lists/:list_id/todos/:todo_id/delete' do
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
 
   todo_id = params[:todo_id].to_i
-  @list[:todos].delete_at(todo_id)
+  @list[:todos].delete_if { |todo| todo[:id] == todo_id }
 
-  session[:success] = 'The todo has been deleted.'
-  redirect "/lists/#{@list_id}"
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
+    status 204
+  else
+    session[:success] = 'The todo has been deleted.'
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Update status of a todo
 post '/lists/:list_id/todos/:todo_id' do
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
 
   todo_id = params[:todo_id].to_i
   is_completed = params[:completed] == 'true'
-  @list[:todos][todo_id][:completed] = is_completed
+  @list[:todos].find { |todo| todo[:id] == todo_id }[:completed] = is_completed
 
   session[:success] = 'The todo has been updated.'
   redirect "/lists/#{@list_id}"
@@ -174,8 +204,7 @@ end
 
 # Mark all todos as complete for a list
 post '/lists/:list_id/complete_all' do
-  @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list_id, @list = load_id_and_list
 
   @list[:todos].each { |todo| todo[:completed] = true }
 
